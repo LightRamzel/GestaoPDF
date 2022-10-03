4a79d76ac512bb1d6413d414b49c939421fca79e
Daniel Polo <daniel_polo@live.com>
2022-10-03 18:16:40 -0300

    [ 01 ] - Criando o Projet em MAUI Blazor

---
+++ GestaoPDF/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GestaoPDF
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ GestaoPDF/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GestaoPDF
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
